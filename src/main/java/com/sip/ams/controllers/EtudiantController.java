c60eae329b01158a84608283caaaad17a8852486
sami <sami.nsira@cgi.com>
2024-09-27 14:37:26 +0200

    DAY 1

--- src/main/java/com/sip/ams/controllers/EtudiantController.java
+++ src/main/java/com/sip/ams/controllers/EtudiantController.java
@@ -1,14 +1,22 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
 public class EtudiantController {
     @RequestMapping("list")
     //@ResponseBody
-	public String  listEtudiants() {
+	public String  listEtudiants(Model model) {
+    	int total =15;
+    	String trainer = "Amine";
+    	model.addAttribute("totalStudent",total);
+    	model.addAttribute("trainerStudent",trainer);
 		return "listeEtudiants";
 	}
 	@RequestMapping("add")
@@ -16,5 +24,11 @@ public class EtudiantController {
 	public String addEtudinat() {
 		return "addEtudiant";
 		
+	}
+	@RequestMapping("SaveEtudiant")
+	@ResponseBody
+	public String saveEtudiant(@RequestParam("nomEtudiant") String nom)
+	{
+		return "vous etes : " +nom;
 	}
 }
